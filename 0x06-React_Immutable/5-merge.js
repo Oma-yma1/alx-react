65d4eb40c33ebc831b214ce937b10cab92d04088
Oma-yma1 <zgani.omayma@gmail.com>
2024-04-25 15:16:12 +0000

    task

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,11 +1,14 @@
-import { Map, List } from 'immutable';
-export function concatElements(page1, page2) {
-  const list1 = List(page1);
-  const list2 = List(page2);
-  return list1.concat(list2);
+import { List, Map } from 'immutable';
+
+// Function to concatenate elements of two arrays
+function concatElements(page1, page2) {
+  return List(page1.concat(page2));
 }
-export function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  return map1.merge(map2);
-};
+
+// Function to merge elements of two objects
+function mergeElements(page1, page2) {
+  const mergedMap = Map(page1).merge(page2);
+  return List(mergedMap.valueSeq());
+}
+
+export { concatElements, mergeElements };
